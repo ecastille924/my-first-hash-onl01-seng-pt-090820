cc0d1cd72198c79b346f6f8938ed107f9f2350e0
Erick Castille <ecastille924@gmail.com>
2020-09-19 17:57:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale_bone_coresets" => 5,  "porcelain vases" => 2, "oil paintings" => 3} 
+  the_manifest = {"whale_bone_corsets" => 5,  "porcelain vases" => 2, "oil paintings" => 3} 
 end
 
 def retrieval
