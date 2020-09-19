5564abc1c61b2fc013425f70e654738502f8c689
Erick Castille <ecastille924@gmail.com>
2020-09-19 18:07:54 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,7 +15,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -29,7 +29,7 @@ shipping_manifest["muskets"] = 2
 
 shipping_manifest["gun powder"] = 4
 
-shipping_manifest["oil paintings"]
+
 end
 
 
